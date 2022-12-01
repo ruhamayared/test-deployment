756a5cb577d9541d62faac5e1852a58de7eb43fe
Ruhama Yared <ruhamayared1@gmail.com>
2022-11-30 19:54:48 -0500

    initial commit

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express();
+const PORT = process.env.PORT || 3002
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+app.get('/dburl', (req, res) => {
+    res.send(`My connection string is: ${process.env.DATABASE_URL}`)
+})
+
+app.listen(PORT, () => {
+    console.log(`✅ PORT: ${PORT} 🌟`); //completely optional
+})
